b77bbae25bdeeb598f7f888265fa145565e0f6c8
Pablo González <pablogonzaleztemes@gmail.com>
2026-04-12 22:20:34 +0200

    feat(Anclaje): :sparkles: add class Anclaje

---
+++ src/main/java/edu/teamrocket/domain/estacion/Anclaje.java
@@ -0,0 +1,34 @@
+package edu.teamrocket.domain.estacion;
+
+import edu.teamrocket.domain.bicicleta.Movil;
+
+public class Anclaje {
+
+    private boolean ocupado = false;
+    private Movil bici = null;
+
+    protected Anclaje() {
+    }
+
+    protected boolean isOcupado() {
+        return this.ocupado;
+    }
+
+    protected Movil getBici() {
+        return this.bici;
+    }
+
+    protected void anclarBici() {
+        this.ocupado = true;
+    }
+
+    protected void librarBici() {
+        this.bici = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Anclaje [ocupado=" + ocupado + ", bici=" + bici + "]";
+    }
+
+}
