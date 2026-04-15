753380f363ae5ec4d1edcc9e414e55d635a5a8f0
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-04-15 13:39:58 +0200

    test(AnclajesTest): :white_check_mark: add AnclajesTest

---
+++ src/test/java/edu/teamrocket/domain/estacion/AnclajesTest.java
@@ -0,0 +1,14 @@
+package edu.teamrocket.domain.estacion;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AnclajesTest {
+
+    @Test
+    public void toStringTest() {
+        Anclajes anclajes = new Anclajes(6);
+        assertEquals("Numero de anclajes: 6", anclajes.toString());
+    } 
+}
