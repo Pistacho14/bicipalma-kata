ba36e61ce57371742d4ecd1132bbc1d5d59859eb
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-04-15 13:48:13 +0200

    feat(TarjetaUsuario): :sparkles: add constructor, getter, setter and toString

--- src/main/java/edu/teamrocket/domain/tarjetausuario/TarjetaUsuario.java
+++ src/main/java/edu/teamrocket/domain/tarjetausuario/TarjetaUsuario.java
@@ -2,4 +2,24 @@ package edu.teamrocket.domain.tarjetausuario;
 
 public class TarjetaUsuario {
     
+    private final String id;
+    private boolean activada;
+
+    public TarjetaUsuario(String id, boolean activada) {
+        this.id = id;
+        this.activada = activada;
+    }
+
+    public boolean isActivada() {
+        return activada;
+    }
+
+    public void setActivada(boolean activada) {
+        this.activada = activada;
+    }
+
+    @Override
+    public String toString() {
+        return "TarjetaUsuario [id=" + id + ", activada=" + activada + "]";
+    }    
 }
